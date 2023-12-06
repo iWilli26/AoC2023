18f06a2360e3b370e76f74fbdec1baee397aaf46
iWilli26 <willi.nguyen@hotmail.fr>
2023-12-06 14:03:00 +0700

    unchanged the input

--- day6/challenge2.py
+++ day6/challenge2.py
@@ -6,12 +6,20 @@ lines = file1.readlines()
 file1.close()
 
 time = re.findall(r'\b\d+\b', lines[0])
+time= list(map(int, time))
+newTime =""
+for i in range(len(time)):
+    newTime = newTime + str(time[i]) 
 
 distance = re.findall(r'\b\d+\b', lines[1])
+distance = list(map(int, distance))
+newDistance =""
+for i in range(len(distance)):
+    newDistance = newDistance + str(distance[i]) 
 
 sum = 0
-timeRace = int(time[0])
-distanceRace = int(distance[0])
+timeRace = int(newTime)
+distanceRace = int(newDistance)
 for second_held in range(timeRace):
     speed = second_held
     distance_traveled = (timeRace - second_held) * speed
